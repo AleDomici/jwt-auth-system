8972b85624b9a41f727e83742c9c600ca56a3f9e
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-02-26 17:31:53 -0300

    feat(config): adicionar configuração de segurança com autenticação e autorização
    Implementada a classe `SecurityConfig` para gerenciar a segurança da aplicação. As principais funcionalidades incluem:
    - Configuração de `SecurityFilterChain` para definir regras de autorização:
      - Rotas `/auth/login` e `/auth/refresh-token` acessíveis sem autenticação.
      - Rota `/admin` restrita a usuários com o papel `ADMIN`.
      - Rota `/user` acessível apenas para usuários autenticados.
    - Configuração de `AuthenticationManager` para autenticação com o serviço `UserService` e codificação de senhas com `BCryptPasswordEncoder`.
    - Bean de `PasswordEncoder` para codificação de senhas.

---
+++ src/main/java/com/catalisa/jwt/security/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.catalisa.jwt.security.config;
+
+
+import com.catalisa.jwt.security.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .dispatcherTypeMatchers("/auth/login", "/auth/refresh-token").permitAll()
+                .dispatcherTypeMatchers("/admin").hasRole("ADMIN")
+                .dispatcherTypeMatchers("/user").authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
